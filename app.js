5794380cbd1572cccc46ae81ab6e3df599866424
Ali Ahmad <turgut@Alis-MacBook-Pro.local>
2022-02-01 18:30:23 +0300

    data added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const theData = require("./data/data");
+const myData = require("./data/data");
 const app = express();
 const PORT = 8000;
 
 app.get("/", (req, res) => {
-  res.json(theData);
+  res.send("Hello World");
+});
+app.get("/api/products", (req, res) => {
+  res.send(myData);
 });
 
 app.listen(PORT, () => {
